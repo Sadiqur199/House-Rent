83d49f281ee162a86e6dd4082e8de4de2926157c
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-07-18 01:25:10 +0600

    added house and search implementation

--- src/api/houses.jsx
+++ src/api/houses.jsx
@@ -9,12 +9,10 @@ const houses = [
   { id: 3, city: 'San Francisco',img:'https://res.akamaized.net/domain/image/upload/t_web/v1629867843/65A_Champion_St_Brighton_VIC_1_bunjuj.jpg', bedrooms: 1, bathrooms: 1, size: '600 sqft', available: false, rent: 1200 },
   { id: 3, city: 'San Francisco',img:'https://res.akamaized.net/domain/image/upload/t_web/v1629867843/65A_Champion_St_Brighton_VIC_1_bunjuj.jpg', bedrooms: 1, bathrooms: 1, size: '600 sqft', available: false, rent: 1200 },
   { id: 3, city: 'San Francisco',img:'https://res.akamaized.net/domain/image/upload/t_web/v1629867843/65A_Champion_St_Brighton_VIC_1_bunjuj.jpg', bedrooms: 1, bathrooms: 1, size: '600 sqft', available: false, rent: 1200 },
-  // Add more house data here...
 ];
 
 const getHouses = () => {
   return new Promise((resolve) => {
-    // Simulate API delay with setTimeout
     setTimeout(() => {
       resolve(houses);
     }, 500);
